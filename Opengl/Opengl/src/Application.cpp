e52f93e68fb8d661bab210c9ff5b12ea32754256
SuperBoyKing <0311pjs@gmail.com>
2020-07-22 23:00:08 +0900

    07.22

--- Opengl/Opengl/src/Application.cpp
+++ Opengl/Opengl/src/Application.cpp
@@ -120,23 +120,31 @@ int main()
 		return -1;
 	}
 
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	ShaderProgramSource shaderSource;
 	shaderSource = ParseShader("res/shader/Basic.shader");
 	unsigned int shaderProgram = CreateShader(shaderSource.vertexShader, shaderSource.fragmentShader);
 
-	float vertices[] = {
+	/*float vertices[] = {
 		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 
 		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
 		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.1f,
 		-0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 0.1f
-	};
-
-	/*float vertices[] = {
-		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.1f
 	};*/
 
+	float vertices[] = {
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 0.1f
+	};
+
+	float texCoords[] = {
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		0.5f, 1.0f
+	};
+	
 	unsigned int indices[] = {
 		0, 1, 3,
 		1, 2, 3
@@ -153,11 +161,12 @@ int main()
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glBindVertexArray(VAO);
+	
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -176,13 +185,13 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 		glUseProgram(shaderProgram);
+		//glUniform4f(vertexColorLocation, 0.0f, r, 0.0f, 1.0f);
 
-		float timeValue = glfwGetTime();
-		float greenValue = sin(timeValue) / 2.0f + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "outColor");
-
-		glUniform4f(vertexColorLocation, r, greenValue, 0.0f, 1.0f);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		if (r > 1.0f)
 			increment = -0.05f;
@@ -190,9 +199,6 @@ int main()
 			increment = 0.05f;
 
 		r += increment;
-
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
