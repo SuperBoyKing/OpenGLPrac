76e5e53dd624f891189c1b60aa97f0ac1c392d7d
SuperBoyKing <0311pjs@gmail.com>
2020-07-28 16:59:31 +0900

    07.28 chpater 9 complete

--- Opengl/Opengl/src/Application.cpp
+++ Opengl/Opengl/src/Application.cpp
@@ -13,7 +13,6 @@
 #include "Renderer.h"
 #include "Shader.h"
 
-
 int main()
 {
 	if (!glfwInit())
@@ -48,123 +47,181 @@ int main()
 	}
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
-
-	Shader shader("res/shader/Basic.shader");
-	//shader.Bind();
-
-
-	float vertices[] = {
-		// position		   // colors		 // texture coords
-		0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
-		0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
-	   -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
-	   -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f
-	};
-	
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3
-	};
-
-	unsigned int VBO;
-	GLCall(glGenBuffers(1, &VBO));
-	
-	unsigned int EBO;
-	GLCall(glGenBuffers(1, &EBO));
-
-	unsigned int VAO;
-	GLCall(glGenVertexArrays(1, &VAO));
-	
-	GLCall(glBindVertexArray(VAO));
-
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, VBO));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
-	
-	GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0));
-	GLCall(glEnableVertexAttribArray(0));
-
-	GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))));
-	GLCall(glEnableVertexAttribArray(1));
-
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	GLCall(glPolygonMode(GL_FRONT_AND_BACK, GL_FILL));
-
-	stbi_set_flip_vertically_on_load(true);
-	unsigned int texture1, texture2;
-	//texture 1
-	///////////;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load("res/textures/container.jpg", &width, &height, &nrChannels, 0);
-
-	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
+		Shader shader("res/shader/Basic.shader");
+		shader.Bind();
+
+		glEnable(GL_DEPTH_TEST);
+
+		float vertices[] = {
+			// position		     // texture coords
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		   -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		};
 
 
-	//texture 2
-	///////////
-	glGenBuffers(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
+		unsigned int VBO, VAO;
+		GLCall(glGenVertexArrays(1, &VAO));
+		GLCall(glGenBuffers(1, &VBO));
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	data = stbi_load("res/textures/awesomeface.png", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
+		GLCall(glBindVertexArray(VAO));
 
-	shader.Bind();
-	glUniform1i(glGetUniformLocation(shader.m_ProgramID, "texture1"), 0);
-	shader.setInt1f("texture2", 1);
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+		GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
 
-	GLCall(glBindVertexArray(0));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+		GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0));
+		GLCall(glEnableVertexAttribArray(0));
 
-	while (!glfwWindowShouldClose(window))
-	{
-		GLCall(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
-		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+		GLCall(glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))));
+		GLCall(glEnableVertexAttribArray(1));
 
-		glActiveTexture(GL_TEXTURE);
+		GLCall(glPolygonMode(GL_FRONT_AND_BACK, GL_FILL));
+
+		stbi_set_flip_vertically_on_load(true);
+		unsigned int texture1, texture2;
+
+		//texture 1
+		//////////////////////////////
+		glGenTextures(1, &texture1);
 		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load("res/textures/container.jpg", &width, &height, &nrChannels, 0);
+
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+
+		//texture 2
+		//////////////////////////////
+		glGenBuffers(1, &texture2);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		shader.Bind();
-		GLCall(glBindVertexArray(VAO));
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-    }
+		data = stbi_load("res/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+
+		shader.Bind();
+		shader.setInt("texture1", 0);
+		shader.setInt("texture2", 1);
+
+		GLCall(glBindVertexArray(0));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+
+		glm::vec3 cubePositions[] = {
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(2.0f, 5.0f, -15.0f),
+			glm::vec3(-1.5f, -2.2f, -2.5f),
+			glm::vec3(-3.8f, -2.0f, -12.3f),
+			glm::vec3(2.4f, -0.4f, -3.5f),
+			glm::vec3(-1.7f, 3.0f, -7.5f),
+			glm::vec3(1.3f, -2.0f, -2.5f),
+			glm::vec3(1.5f, 2.0f, -2.5f),
+			glm::vec3(1.5f, 0.2f, -1.5f),
+			glm::vec3(-1.3f, 1.0f, -1.5f)
+		};
+
+		while (!glfwWindowShouldClose(window))
+		{
+			GLCall(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
+			GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+
+			glActiveTexture(GL_TEXTURE);
+			glBindTexture(GL_TEXTURE_2D, texture1);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, texture2);
+
+			
+			glm::mat4 view = glm::mat4(1.0f);
+			glm::mat4 projection = glm::mat4(1.0f);
+
+			//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+			projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+			shader.setMat4("view", view);
+			shader.setMat4("projection", projection);
+
+			shader.Bind();
+			GLCall(glBindVertexArray(VAO));
+			for (unsigned int i = 0; i < 10; i++)
+			{
+				glm::mat4 model = glm::mat4(1.0f);
+				model = glm::translate(model, cubePositions[i]);
+				float angle = 20.0f * i;
+				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.5f));
+				shader.setMat4("model", model);
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+	}
+
 	glfwTerminate();
 	return 0;
 }
